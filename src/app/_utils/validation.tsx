1a922897f96e5fdbbb6204b2c54b7a9c3384b346
hun2__2 <wognskec@hanyang.ac.kr>
2023-12-11 15:45:36 +0900

    [feat] IPaddress scope validation 추가
    1. 같거나 클때만 true 반환

--- src/app/_utils/validation.tsx
+++ src/app/_utils/validation.tsx
@@ -1,14 +1,29 @@
+// 192.168.1.1
 export const validatedIpAddress = (ipAddress: string) => {
   const ipRegex =
     /^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9])){3}$/;
   return ipRegex.test(ipAddress);
 };
 
+// 00:1A:2B:3C:4D:5E
 export const validatedMACAddress = (macAddress: string) => {
   const macRegex = /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/;
   return macRegex.test(macAddress);
 };
-export const validatedScopeIPAddress = () => {};
+
+export const validatedScopeIPAddress = (start: string, end: string) => {
+  const ipToNumberArray = (ip: string) => ip.split('.').map(Number);
+
+  const startIP = ipToNumberArray(start);
+  const endIP = ipToNumberArray(end);
+
+  for (let i = 0; i < 4; i++) {
+    if (startIP[i] <= endIP[i]) continue;
+    else return false;
+  }
+
+  return true;
+};
 
 // interface FormDataProps{
 //   ip : string
